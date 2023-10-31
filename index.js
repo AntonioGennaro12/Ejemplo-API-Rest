ddc86abd7c192c8bef56da4ca803608c34e15087
Antonio Gennaro <antgennaro@hotmail.com>
2023-10-31 07:46:56 -0300

    Inicialización completa

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express"
 const app = express();
 
+app.get("/",(req, res)=>{
+    console.log("get");
+    res.send("<h1> Hola </h1>")
+}); 
+
+
 app.listen(5000, ()=>{
     console.log("Servidor esta activo");
 });
 
 app.use((req, res)=>{
-    res.send("<h1> ERROR: ruta no existe</h>")
-});
+    res.send("<h1> ERROR: ruta no existe</h1>")
+}); 
 
