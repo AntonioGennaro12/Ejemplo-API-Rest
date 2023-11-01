767cb5a66fab1b76f3471f4168a92c38bd68d14a
Antonio Gennaro <antgennaro@hotmail.com>
2023-11-01 17:46:12 -0300

    Rutas iniciales completadas

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 import express from "express"
 const app = express();
 
-app.get("/",(req, res)=>{
-    console.log("get");
-    res.send("<h1> Hola </h1>")
+app.use(express.json());
+
+app.get("/api/",(req, res)=>{
+    console.log(req.query);
+    res.send(`<h1> Hola 1 ${req.query.param2}</h1>`)
 }); 
 
+app.get("/api/:id",(req, res)=>{
+    console.log(req.params);
+    res.send("<h1> Hola 2 </h1>")
+}); 
+
+app.post("/api",(req, res)=>{
+    console.log(req.body);
+    // debería crear algo en la base de datos
+    res.status(201).json(req.body.info3);
+})
+
+app.post("/api/:nombre",(req, res)=>{
+    // crea pidiendo info en la url
+    res.status(201).send(`
+    <h2> ${req.params.nombre}</h2> `
+    );
+})
+
+app.put("/api",(req, res)=>{
+    // debería crear algo en la base de datos
+    res.json({
+    mensaje: "put todo OK" 
+    });
+})
+
+
+app.delete("/api",(req, res)=>{
+    // debería crear algo en la base de datos
+    res.json({
+    mensaje: "delete todo OK" 
+    });
+})
+
+// http://localhost:5000/api?param=false&param2=%22otrovalor%22
 
 app.listen(5000, ()=>{
     console.log("Servidor esta activo");
